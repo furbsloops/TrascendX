24050915260e4f8d3d23cef4a9cd51e12df45e0d
furbsloops <furbsloops@gmail.com>
2024-12-10 00:30:05 +0100

    apikeynft

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { motion } from 'framer-motion';
 import './Gallery.css'; // Importa il tuo file CSS
 
 const Gallery = () => {
-  const nfts = Array.from({ length: 120 }, (_, index) => ({
-    id: index,
-    name: `NFT ${index + 1}`,
-    description: 'This is a placeholder NFT.',
-    imageUrl: `https://picsum.photos/seed/${index}/200`
-  }));
+  const [nfts, setNfts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.opensea.io/api/v1/assets', {
+      params: {
+        asset_contract_address: '0x55ca8C033F230Ef9dd09412784948a9D4734DccF', // L'indirizzo del contratto NFT
+        order_direction: 'desc',
+        limit: 20 // Numero di NFT da mostrare
+      },
+      headers: {
+        'X-API-KEY': 'c65c1eaf50f34b1e83ea01b163ca0711' // Sostituisci con la tua API Key di OpenSea
+      }
+    })
+    .then(response => {
+      const fetchedNFTs = response.data.assets || [];
+      setNfts(fetchedNFTs);
+    })
+    .catch(error => {
+      console.error('Errore nel fetch degli NFT da OpenSea:', error);
+    });
+  }, []);
 
   return (
     <div className="gallery-container">
-      {nfts.map(nft => (
+      {nfts.length > 0 ? nfts.map(nft => (
         <motion.div
           key={nft.id}
           whileHover={{ scale: 1.05 }}
@@ -21,13 +37,15 @@ const Gallery = () => {
           transition={{ duration: 0.5 }}
           className="nft-card"
         >
-          <img src={nft.imageUrl} alt={nft.name} className="nft-image"/>
+          <img src={nft.image_url} alt={nft.name} className="nft-image"/>
           <div className="nft-info">
             <h5>{nft.name}</h5>
             <p>{nft.description}</p>
           </div>
         </motion.div>
-      ))}
+      )) : (
+        <p>Nessun NFT trovato per questa collezione.</p>
+      )}
     </div>
   );
 };
