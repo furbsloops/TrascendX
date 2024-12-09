f44f5cbc6304bf99f47ef4c7f96d224333a7a6db
furbsloops <furbsloops@gmail.com>
2024-12-09 22:33:13 +0100

    APIOpenSea

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { motion } from 'framer-motion';
 
 const Gallery = () => {
-  const nfts = Array.from({ length: 120 }, (_, index) => ({
-    id: index,
-    name: `NFT ${index + 1}`,
-    description: 'This is a placeholder NFT.',
-    imageUrl: `https://picsum.photos/seed/${index}/200`
-  }));
+  const [nfts, setNfts] = useState([]);
+
+  useEffect(() => {
+    const fetchNFTs = async () => {
+      try {
+        // Modifica con il tuo endpoint API effettivo
+        const response = await axios.get('https://api.opensea.io/api/v1/assets', {
+          params: {
+            owner: '0xFeFbd651De672716Da4e9bf90726249151DEdd09', // Sostituisci con l'indirizzo wallet reale
+            order_direction: 'desc',
+            limit: 50
+          },
+          headers: {
+            'X-API-KEY': 'c65c1eaf50f34b1e83ea01b163ca0711' // Inserisci qui la tua API key effettiva
+          }
+        });
+        setNfts(response.data.assets);
+      } catch (error) {
+        console.error('Error fetching NFTs:', error);
+      }
+    };
+
+    fetchNFTs();
+  }, []);
 
   return (
     <div className="p-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4">
@@ -20,10 +39,10 @@ const Gallery = () => {
           transition={{ duration: 0.5 }}
           className="overflow-hidden shadow-lg rounded-lg cursor-pointer"
         >
-          <img src={nft.imageUrl} alt={nft.name} className="w-full h-auto"/>
+          <img src={nft.image_url || nft.image_preview_url} alt={nft.name} className="w-full h-auto"/>
           <div className="p-4 bg-gray-900 text-white">
             <h5 className="text-lg font-bold">{nft.name}</h5>
-            <p>{nft.description}</p>
+            <p>{nft.description || 'No description available.'}</p>
           </div>
         </motion.div>
       ))}
